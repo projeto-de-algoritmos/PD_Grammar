960fc30d486bce8dd97cc41d24532329ed20e90c
dougAlvs <douglasvestos@gmail.com>
2023-12-07 19:29:29 -0300

    feat: implementa a Trie, seleção do valor do gap e benchmarks da busca

--- api/util.py
+++ api/util.py
@@ -1,56 +1,73 @@
-def levenshtein_distance(str1, str2, gap, mismatchs_dict):
-    len_str1 = len(str1) + 1
-    len_str2 = len(str2) + 1
-
-    str1 = str1.lower()
-    str2 = str2.lower()
-
-    # Inicialização da matriz
-    matrix = [[0 for _ in range(len_str2)] for _ in range(len_str1)]
-
-    # Preenchimento da matriz com valores iniciais dos gaps
-    for i in range(len_str1):
-        matrix[i][0] = i * gap
-
-    # Gaps na segunda string (dicionario) tem peso infinito
-    for j in range(1, len_str2):
-        matrix[0][j] = float('inf')
-
-    # Cálculo da distância de edição
-    for i in range(1, len_str1):
-        for j in range(1, len_str2):
-            matrix[i][j] = min(
-                matrix[i - 1][j - 1] + mismatchs_dict.get((str1[i - 1], str2[j - 1])),  # Mismatch
-                matrix[i][j - 1] + gap,                # Gap em str1
-                float('inf')                           # Gap em str2
-            )
-
-    return matrix[len_str1 - 1][len_str2 - 1]
+class TrieNode:
+    def __init__(self):
+        self.word = None
+        self.children = {}
 
 
-def auto_correct(word, word_list, gap, mismatchs_dict):
-    min_distance = float('inf')
-    closest_word = ''
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        self.mismatch_dict = self.__get_mismatch_dict()
 
-    for candidate in word_list:
-        distance = levenshtein_distance(word, candidate, gap, mismatchs_dict)
-        if distance < min_distance:
-            min_distance = distance
-            closest_word = candidate
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
 
-    return closest_word
+            node = node.children[char]
 
+        node.word = word
 
-def get_mismatch_dict():
-    # Criação do dicionário de pesos de mismatch
-    keyboard = [
-        ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
-        ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ç'],
-        ['z', 'x', 'c', 'v', 'b', 'n', 'm']
-    ]
+    def __get_mismatch_dict(self):
+        # Criação do dicionário de pesos de mismatch
+        keyboard = [
+            ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+            ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ç'],
+            ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+        ]
 
-    key_positions = {key: (i, j) for i, row in enumerate(keyboard) for j, key in enumerate(row)}
+        key_positions = {key: (i, j) for i, row in enumerate(keyboard) for j, key in enumerate(row)}
 
-    mismatchs_dict = {(key1, key2): abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1]) for key1, pos1 in key_positions.items() for key2, pos2 in key_positions.items()}
+        mismatchs_dict = {(key1, key2): abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1]) for key1, pos1 in key_positions.items() for key2, pos2 in key_positions.items()}
 
-    return mismatchs_dict
+        return mismatchs_dict
+
+    def auto_correct(self, word, maxCost, gapCost):
+        # Preenchimento da primeira linha da matriz com o
+        currentRow = [i * gapCost for i in range(len(word) + 1)]
+
+        results = []
+
+        global visited_nodes
+        visited_nodes = 0
+
+        # Pesquisa cada ramo da trie recursivamente
+        for char in self.root.children:
+            self.levenshtein_distance(self.root.children[char], char, word, currentRow, results, maxCost, gapCost)
+
+        return (min(results, key=lambda x: x[1]), visited_nodes) if results else ([], visited_nodes)
+
+    def levenshtein_distance(self, node, char, word, previousRow, results, maxCost, gapCost):
+        global visited_nodes
+        visited_nodes += 1
+        cols = len(word) + 1
+
+        # O custo de gaps nas palavras do dicionario é infinito, pois elas já estão corretas
+        currentRow = [float("inf")]
+
+        # Cada linha é um novo caractere da trie e cada coluna um caractere da palavra a ser corrigida
+        for col in range(1, cols):
+            gap = previousRow[col] + gapCost
+            mismatch = previousRow[ col - 1 ] + self.mismatch_dict.get((word[col - 1], char))
+
+            currentRow.append(min(gap, mismatch))
+
+        # Caso haja uma palavra já formada com custo menor do que o parâmetro maxCost, adiciona ela ao resultado
+        if currentRow[-1] <= maxCost and node.word is not None:
+            results.append((node.word, currentRow[-1]))
+
+        # Caso haja alguma célula com custo menor do que o parâmetro maxCost, chama a função recursivamente para aquele ramo da trie
+        if min(currentRow) <= maxCost:
+            for char in node.children:
+                self.levenshtein_distance(node.children[char], char, word, currentRow, results, maxCost, gapCost)
--- api/views.py
+++ api/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render
-from .util import auto_correct, get_mismatch_dict
+from .util import Trie
 from .forms import TextCheckForm
+import time
+import datetime
 
 
 def readDict():
-    with open('word_dict.txt', 'r') as f:
-        word_dict = f.read().splitlines()
+    trie = Trie()
 
-    return word_dict
+    with open("./word_dict.txt", 'r') as f:
+        for w in f.read().split():
+            trie.insert(w.lower())
+
+    return trie
+
+
+word_dict = None
 
 
 def index(request):
-    word_dict = readDict()
-    mismatch_dict = get_mismatch_dict()
-    gap_weight = 5
+    # Se ainda não tiver instanciado o dicionário, instancia
+    global word_dict
+    if word_dict is None:
+        word_dict = readDict()
 
     closest_word = ''
     input_word = ''
@@ -24,11 +33,19 @@ def index(request):
             form = TextCheckForm()
             form.fields["word"].initial = ''
         if form.is_valid():
-            input_word = form.cleaned_data['word']
-            closest_word = auto_correct(input_word, word_dict, gap_weight, mismatch_dict)
+            input_word = form.cleaned_data['word'].lower()
+            gap_cost = form.cleaned_data['gap_cost']
+            max_diff = form.cleaned_data['max_diff']
 
-            return render(request, 'api/index.html', {'form': form, 'input_word': input_word, 'closest_word': closest_word})
+            tic = time.time()
+            closest_word, visited_nodes = word_dict.auto_correct(input_word, max_diff, gap_cost)
+            tac = time.time()
+
+            # Formatando o tempo gasto em um formato legível
+            time_passed = " minutos e ".join(str(datetime.timedelta(seconds=tac - tic))[2:-3].split(":")) + " segundos"
+
+            return render(request, 'api/index.html', {'form': form, 'input_word': input_word, 'closest_word': closest_word, 'time_passed': time_passed, 'visited_nodes': visited_nodes})
     else:
-        form = TextCheckForm(initial='')
-
+        form = TextCheckForm()
+        
     return render(request, 'api/index.html', {'form': form, 'input_word': input_word, 'closest_word': closest_word})
